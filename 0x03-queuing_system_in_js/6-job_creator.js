f06b976a5cf4967757d8551265cc0e42238f49ea
nadnz080 <nandz080@gmail.com>
2024-06-12 13:31:49 +0000

    [task 6] Create a queue named push_notification_code and create a job with the object created before

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+// Define the job data
+const jobData = {
+    phoneNumber: '1234567890',
+    message: 'Hello, this is a notification!'
+};
+
+// Create a job in the queue
+const job = queue.create('push_notification_code', jobData)
+    .save((err) => {
+        if (!err) {
+            console.log(`Notification job created: ${job.id}`);
+        }
+    });
+
+// Event handlers
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+    console.log('Notification job failed');
+});
