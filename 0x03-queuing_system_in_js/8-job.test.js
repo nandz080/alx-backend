d713f8838b1f063f00eafe0510481c813a5b69a9
nandz080 <nandz080@gmail.com>
2024-07-31 20:55:08 +0000

    [task 11]

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,10 +1,13 @@
 // Import necessary modules
 import { expect } from 'chai';
+
 import kue from 'kue';
+
 import createPushNotificationsJobs from './8-job.js';
 
 // Set up the Kue queue
 const queue = kue.createQueue();
+
 
 // Enter test mode
 queue.testMode.enter();
@@ -15,14 +18,17 @@ describe('createPushNotificationsJobs', () => {
     queue.testMode.clear();
   });
 
+
   // Exit test mode after all tests are done
   after(() => {
     queue.testMode.exit();
   });
 
+
   it('should display an error message if jobs is not an array', () => {
     expect(() => createPushNotificationsJobs(null, queue)).to.throw('Jobs is not an array');
   });
+
 
   it('should create two new jobs to the queue', () => {
     // Define the array of jobs
@@ -36,6 +42,7 @@ describe('createPushNotificationsJobs', () => {
         message: 'This is the code 4562 to verify your account'
       }
     ];
+
 
     // Call the function to create jobs
     createPushNotificationsJobs(jobs, queue);
