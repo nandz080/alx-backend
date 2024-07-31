d7831a354a4303d24b1b69b4c5900f44f08078b0
nandz080 <nandz080@gmail.com>
2024-07-31 20:26:19 +0000

    [task 5] create a redis client

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -5,13 +5,15 @@ client.on('connect', () => {
     console.log('Redis client connected to the server');
 });
 
-client.on('error', (err) => {
-    console.log('Redis client not connected to the server:', err);
+client
+	.on('error', (err) => {
+    	console.log('Redis client not connected to the server:', err);
 });
 
 client.subscribe('holberton school channel');
 
-client.on('message', (channel, message) => {
+client
+    .on('message', (channel, message) => {
     console.log(message);
     if (message === 'KILL_SERVER') {
         client.unsubscribe();
