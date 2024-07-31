ec2de1891e371dda27bc982de5303a6884cd346a
nandz080 <nandz080@gmail.com>
2024-07-31 20:51:56 +0000

    [task 10] create a function named createPushNotification

--- 0x03-queuing_system_in_js/8-job-main.js
+++ 0x03-queuing_system_in_js/8-job-main.js
@@ -1,8 +1,11 @@
 import kue from 'kue';
 
+
 import createPushNotificationsJobs from './8-job.js';
 
+
 const queue = kue.createQueue();
+
 
 const list = [
     {
@@ -10,4 +13,6 @@ const list = [
     message: 'This is the code 1234 to verify your account'
     }
 ];
+
+
 createPushNotificationsJobs(list, queue);
--- 0x03-queuing_system_in_js/8-jobs.js
+++ 0x03-queuing_system_in_js/8-jobs.js
@@ -1,8 +1,10 @@
 // Import the Kue library
 import kue from 'kue';
 
+
 // Define the function createPushNotificationsJobs
 const createPushNotificationsJobs = (jobs, queue) => {
+    
     // Check if jobs is an array
     if (!Array.isArray(jobs)) {
         throw new Error('Jobs is not an array');
@@ -10,7 +12,8 @@ const createPushNotificationsJobs = (jobs, queue) => {
 
     // Iterate over each job in the array
     jobs.forEach((jobData) => {
-        // Create a job in the queue push_notification_code_3
+        
+	// Create a job in the queue push_notification_code_3
         const job = queue.create('push_notification_code_3', jobData);
 
         // Event listener for job creation
@@ -18,25 +21,31 @@ const createPushNotificationsJobs = (jobs, queue) => {
             console.log(`Notification job created: ${job.id}`);
         });
 
+
         // Event listener for job completion
         job.on('complete', () => {
             console.log(`Notification job ${job.id} completed`);
         });
+
 
         // Event listener for job failure
         job.on('failed', (errorMessage) => {
             console.log(`Notification job ${job.id} failed: ${errorMessage}`);
         });
 
+
         // Event listener for job progress
         job.on('progress', (progress, data) => {
             console.log(`Notification job ${job.id} ${progress}% complete`);
         });
 
+
         // Save the job to the queue
         job.save();
     });
+
 };
+
 
 // Export the createPushNotificationsJobs function
 export default createPushNotificationsJobs;
